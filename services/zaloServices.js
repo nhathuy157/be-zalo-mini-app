2b8f7529d3ca872e9051cc0ff43677a162e41fc7
Nhật Huy <89715333+nhathuy157@users.noreply.github.com>
2025-01-03 16:25:00 +0700

    up
    up

--- services/zaloServices.js
+++ services/zaloServices.js
@@ -5,14 +5,33 @@ const service = {};
 const API_DOMAIN = 'https://graph.zalo.me';
 const OPEN_API_DOMAIN = 'https://openapi.zalo.me'
 
+// service.getZaloProfile = (accessToken) => {
+//     return new Promise((resolve, reject) => {
+//         request({
+//             url: `${API_DOMAIN}/v2.0/me`,
+//             method: 'GET',
+//             qs: {
+//                 access_token: accessToken,
+//                 fields: 'id,name,birthday,email,picture'
+//             },
+//             json: true
+//         }, (error, response, body) => {
+//             if (error) return reject(error);
+//             return resolve(body);
+//         });
+//     });
+// }
+
 service.getZaloProfile = (accessToken) => {
     return new Promise((resolve, reject) => {
         request({
             url: `${API_DOMAIN}/v2.0/me`,
             method: 'GET',
+            headers: {
+                Authorization: `Bearer ${accessToken}` // Đặt AccessToken trong header
+            },
             qs: {
-                access_token: accessToken,
-                fields: 'id,name,birthday,email,picture'
+                fields: 'id,name,birthday,email,picture' // Chỉ truyền các tham số cần thiết
             },
             json: true
         }, (error, response, body) => {
@@ -21,6 +40,7 @@ service.getZaloProfile = (accessToken) => {
         });
     });
 }
+
 
 service.sendMessage = (userId, text) => {
     return new Promise((resolve, reject) => {
